575c0c21357800b62881ddade5332af855f62fc5
NathanP11 <156483912+NathanP11@users.noreply.github.com>
2024-02-16 17:06:39 -0600

    Add files via upload

---
+++ Cw_W2_Softmax.py
@@ -0,0 +1,37 @@
+# UNQ_C1
+# GRADED CELL: my_softmax
+
+def my_softmax(z):  
+    """ Softmax converts a vector of values to a probability distribution.
+    Args:
+      z (ndarray (N,))  : input data, N features
+    Returns:
+      a (ndarray (N,))  : softmax of z
+    """    
+    ### START CODE HERE ### 
+    
+    a = np.zeros(z.shape)
+    myExp = np.exp(z)
+    for i in range(z.shape[0]):
+
+    ### END CODE HERE ### 
+    return a
+	
+	
+	
+	# UNQ_C2
+# GRADED CELL: Sequential model
+tf.random.set_seed(1234) # for consistent results
+model = Sequential(
+    [               
+        ### START CODE HERE ### 
+        Dense(25, activation = 'relu'),
+        Dense(15, activation = 'relu'),
+        Dense(10, activation = 'linear')
+        
+        ### END CODE HERE ### 
+    ], name = "my_model" 
+)
+
+
+	
