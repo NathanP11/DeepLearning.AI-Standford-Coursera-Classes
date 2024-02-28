aa66d7afd15125d7f09beac1563bdc4fc418a118
NathanP11 <156483912+NathanP11@users.noreply.github.com>
2024-02-27 18:45:49 -0600

    Add files via upload

---
+++ c3_W2_RecSySNN_2x_matricies.py
@@ -0,0 +1,56 @@
+
+# GRADED_CELL
+# UNQ_C1
+
+num_outputs = 32
+tf.random.set_seed(1)
+user_NN = tf.keras.models.Sequential([
+    ### START CODE HERE ###     
+    tf.keras.layers.Dense(256, activation = 'relu'           ),#     ,    kernel_regularizer=regularizers.L2(lambda_))
+    tf.keras.layers.Dense(128, activation = 'relu'           ),#     ,    kernel_regularizer=regularizers.L2(lambda_))
+    tf.keras.layers.Dense(num_outputs, activation = 'linear' )#     ,    kernel_regularizer=regularizers.L2(lambda_))
+    ### END CODE HERE ###  
+])
+
+item_NN = tf.keras.models.Sequential([
+    ### START CODE HERE ###     
+    tf.keras.layers.Dense(256, activation = 'relu'           ),#     ,    kernel_regularizer=regularizers.L2(lambda_))
+    tf.keras.layers.Dense(128, activation = 'relu'           ),#     ,    kernel_regularizer=regularizers.L2(lambda_))
+    tf.keras.layers.Dense(num_outputs, activation = 'linear' )#     ,    kernel_regularizer=regularizers.L2(lambda_))
+    ### END CODE HERE ###  
+])
+
+# create the user input and point to the base network
+input_user = tf.keras.layers.Input(shape=(num_user_features))
+vu = user_NN(input_user)
+vu = tf.linalg.l2_normalize(vu, axis=1)
+
+# create the item input and point to the base network
+input_item = tf.keras.layers.Input(shape=(num_item_features))
+vm = item_NN(input_item)
+vm = tf.linalg.l2_normalize(vm, axis=1)
+
+# compute the dot product of the two vectors vu and vm
+output = tf.keras.layers.Dot(axes=1)([vu, vm])
+
+# specify the inputs and output of the model
+model = tf.keras.Model([input_user, input_item], output)
+
+model.summary()
+
+
+# GRADED_FUNCTION: sq_dist
+# UNQ_C2
+def sq_dist(a,b):
+    """
+    Returns the squared distance between two vectors
+    Args:
+      a (ndarray (n,)): vector with n features
+      b (ndarray (n,)): vector with n features
+    Returns:
+      d (float) : distance
+    """
+    ### START CODE HERE ###     
+    d = np.sum( np.square( a - b ) )
+    ### END CODE HERE ###     
+    return d
